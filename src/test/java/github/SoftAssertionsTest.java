6caba715a96bb3d1e79e6a135fba8d8540fb7f3a
Kolyshkin A <t0l4ik.kas1@gmail.com>
2025-04-14 22:18:33 +0500

    add assertionstest

---
+++ src/test/java/github/SoftAssertionsTest.java
@@ -0,0 +1,35 @@
+package github;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SoftAssertionsTest {
+
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void successfulSoftAssertionsTest(){
+
+        open("selenide/selenide");
+        $(".UnderlineNav-body").$(byText("Wiki")).click();
+
+        $$("a.internal.present").findBy(text("Soft assertions"))
+                .shouldBe(visible).click();
+
+        $(".markdown-body").$$("ol li").findBy(text("JUnit5"))
+                .shouldHave(text("JUnit5"));
+
+
+    }
+}
